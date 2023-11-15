5fed46f82338d2b2b3992efb6f8f43871afb464a
Bogdanendre <148947668+Bogdanendre@users.noreply.github.com>
2023-11-15 10:45:26 +0200

    Add files via upload

---
+++ for3feladat/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,S=0;
+    cout<<"n=";
+    cin>>n;
+    for(i=1;i<=n;i+=2){
+
+            S=S+i;
+
+    }
+    cout<<S;
+    return 0;
+}
